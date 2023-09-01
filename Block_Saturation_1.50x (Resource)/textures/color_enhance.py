af8ef557fa3c29a12b1f9e1d29424bd7525045f0
bud-aj29 <99773087+bud-aj29@users.noreply.github.com>
2023-09-01 13:04:03 +0900

    v0.0.1
    Initial commit

---
+++ Block_Saturation_1.50x (Resource)/textures/color_enhance.py
@@ -0,0 +1,20 @@
+# import required module
+from PIL import Image
+from PIL import ImageEnhance
+import os
+ 
+# assign directory
+directory = os.getcwd()
+ 
+# iterate over files in that directory
+for root, dirs, files in os.walk(directory):
+    for filename in files:
+        #print(os.path.join(root, filename))
+        f_name = os.path.join(root, filename)
+        if filename.endswith(('.png', '.tga')):
+            img = Image.open(f_name)
+            img2 = img.convert("RGBA")
+            converter = ImageEnhance.Color(img2)
+            img3 = converter.enhance(1.5)
+            img3.save(f_name)
+
